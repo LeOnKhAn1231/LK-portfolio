436d05572ed1d9e84d01a343afc81c774766a5d0
muhammadleon <muhammadleon1231@gmail.com>
2023-08-29 22:17:53 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+
+// MIX IT UP CONTENT
+var mixer = mixitup('.work-grid');
+
+//js for ham-menu
+function openMenu(){
+    document.getElementById('menu-container').style.width = "100%";
+}
+function closeMenu(){
+    document.getElementById('menu-container').style.width = "0";
+}
+new WOW().init();
